f0df4ffe4f6d02219ac0d163c1386c5f3ffd1b98
GulMadred <edwardqjones@hotmail.com>
2025-02-02 11:15:44 -0500

    New option to suppress ideo-related error spam

---
+++ Source/BreedingRitual/Patches/Patch_CompAssignableToPawn_Bed_IdeoligionForbids.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace BreedingRitual.Patches
+{
+    [HarmonyPatch(typeof(CompAssignableToPawn_Bed), nameof(CompAssignableToPawn_Bed.IdeoligionForbids))]
+    public static class Patch_CompAssignableToPawn_Bed_IdeoligionForbids
+    {
+        public static void Postfix(ref bool __result, ref Pawn pawn)
+        {
+            // Does the player want us to suppress ideoligion error messages?
+            if (BreedingRitual.BreedingRitualSettings.suppressIdeoErrors)
+            {
+                // Yes. The game has already decided whether Ideoligion rules
+                // would forbid this action. Simply overwrite its answer.
+                __result = false;
+                return;
+            }
+            else
+            {
+                // No. Let the original result stand.
+            }
+        }
+    }
+}
