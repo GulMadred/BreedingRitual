e000e31f23b8f7760fc5c09eeb27e286b0a85f47
GulMadred <edwardqjones@hotmail.com>
2025-02-02 11:11:53 -0500

    New class. Reports Anima grass consumption

---
+++ Source/BreedingRitual/RitualOutcomeEffectWorker_Animabreeding.cs
@@ -0,0 +1,28 @@
+﻿using Verse;
+
+namespace RimWorld
+{
+    public class RitualOutcomeEffectWorker_Animabreeding : RitualOutcomeEffectWorker_Breeding
+    {
+        public RitualOutcomeEffectWorker_Animabreeding()
+        {
+        }
+
+        public RitualOutcomeEffectWorker_Animabreeding(RitualOutcomeEffectDef def)
+            : base(def)
+        {
+        }
+
+        // Animabreeding doesn't rely on fitness; couples will always do exactly ONE round of Lovin'.
+        // Hence, it would be confusing/misleading to tell the player about their "poor" performance
+        protected override bool ReportFitness()
+        {
+            return false;
+        }
+
+        protected override string SupplementalReport()
+        {
+            return "MessageAnimabreedingReport".Translate(LordJob_AnimabreedingRitual.animaGrassConsumed.Named("COST"), LordJob_AnimabreedingRitual.animaGrassConserved.Named("REFUND")).CapitalizeFirst();
+        }
+    }
+}
